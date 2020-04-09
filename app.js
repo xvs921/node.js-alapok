873d8152afb4e130846ad3c7eb4540b41c7650ff
VorakGergo <vorakgeri@gmail.com>
2020-04-09 14:07:31 +0200

    16

--- app.js
+++ app.js
@@ -30,4 +30,12 @@ app.listen(3000);*/
 const fs=require('fs');
 const fileName="target.txt";
 
-fs.watch(fileName,()=>console.log('File changed!'));
+//fs.watch(fileName,()=>console.log('File changed!'));
+fs.readFile(fileName, (err,data) => {
+    if(err){
+        console.log(err);
+    }
+    console.log(data.toString());
+});
+
+console.log("Node js async programming...?");
