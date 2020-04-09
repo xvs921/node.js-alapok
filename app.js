c679aeff6a7fee38fa31b6f59a0d14ef1c933774
VorakGergo <vorakgeri@gmail.com>
2020-04-09 14:12:07 +0200

    17

--- app.js
+++ app.js
@@ -26,16 +26,21 @@ app.get('/',(req,res)=>{
 });
 
 app.listen(3000);*/
+
 //15 EVENT LOOP
 const fs=require('fs');
 const fileName="target.txt";
 
+const data=fs.readFileSync(fileName);
+console.log(data.toString());
+
 //fs.watch(fileName,()=>console.log('File changed!'));
+/*16 ASZINKRON
 fs.readFile(fileName, (err,data) => {
     if(err){
         console.log(err);
     }
     console.log(data.toString());
-});
+});*/
 
 console.log("Node js async programming...?");
